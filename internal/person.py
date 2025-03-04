8765784c6882c8cf7b9be417dac02e2f1d82d9ed
Artem <119163335+ARTEMSROSLOV@users.noreply.github.com>
2025-03-04 20:09:38 +0300

    person.py
    рыцарь

---
+++ internal/person.py
@@ -0,0 +1,15 @@
+class Person:
+  def __init__(self, name, health, armor, weapon):
+    self.name = name
+    self.health = healh
+    self.armor = armor
+    self.weapon = weapon
+
+class Knight(Person):
+  def __init__(self, name, health, armor, weapon):
+    self.name = name
+    self.health = healh
+    self.armor = armor
+    self.weapon = weapon
+
+
